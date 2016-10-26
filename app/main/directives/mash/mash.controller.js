fffbbd7c79c52407dc0c365c3e200ef53b397e8a
RicardViES <ricardvies@gmail.com>
2016-10-26 12:45:27 +0200

    fix test

--- app/main/directives/mash/mash.controller.js
+++ app/main/directives/mash/mash.controller.js
@@ -1,3 +1,45 @@
-/**
- * Created by ricky on 25/10/16.
- */
+'use strict';
+
+(function () {
+  angular
+    .module('batchControl')
+    .controller('BCMash', BCMash);
+  function BCMash ($log) {
+
+    var logger = $log;
+    var msh = this;
+    msh.init = init;
+    msh.processNumberOfSteps = processNumberOfSteps;
+    msh.createMashWatch = createMashWatch;
+
+    msh.showStepsForm = true;
+    msh.showMashWatch = false;
+    msh.numberOfSteps = 1;
+    msh.steps = [{'duration': '', 'temperature': ''}];
+    msh.wrongStepNum = false;
+
+    init();
+
+    function init () {
+      logger.log('Starting mash controller');
+    }
+
+    function processNumberOfSteps () {
+      if (!angular.isDefined(msh.numberOfSteps) || (msh.numberOfSteps < 1 || msh.numberOfSteps > 4)) {
+        logger.log('Number of steps out of range');
+        msh.wrongStepNum = true;
+      } else {
+        logger.log('number of steps : ', msh.numberOfSteps);
+        msh.showStepsForm = false;
+        msh.wrongStepNum = false;
+      }
+    }
+
+    function createMashWatch () {
+      logger.log('creating mash watch with : ', msh.steps);
+      msh.showMashWatch = true;
+
+    }
+
+  }
+})();
--- app/main/directives/mash/mash.directive.js
+++ app/main/directives/mash/mash.directive.js
@@ -1,3 +1,28 @@
-/**
- * Created by ricky on 25/10/16.
- */
+'use strict';
+
+(function () {
+
+  angular
+    .module('batchControl')
+    .directive('bCDMash', BCDMash);
+
+  /* @ngInject */
+  function BCDMash ($log) {
+    var logger = $log;
+    logger.debug('init BCDMash');
+    var directive = {
+      controller: 'BCMash',
+      bindToController: true,
+      controllerAs: 'msh',
+      templateUrl: 'main/directives/mash/mash.template.html',
+      restrict: 'E',
+      transclude: true
+    };
+    return directive;
+
+
+  }
+
+})();
+
+
